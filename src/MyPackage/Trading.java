fd7d82f4ca3651b3cbbef27a95088df200ec7013
Shaik Anees <71702294+testadmin155@users.noreply.github.com>
2023-07-07 07:01:57 +0200

    Update Trading.java

--- src/MyPackage/Trading.java
+++ src/MyPackage/Trading.java
@@ -3,7 +3,7 @@ package MyPackage;
 public class Trading {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is a test case for Trading");
 
 	}
 
