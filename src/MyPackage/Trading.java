d216c7f41517b1c93f86739ef6272c3cef5d1966
Anees <skanees007@gmail.com>
2023-07-07 07:16:26 +0200

    Sixth Commit

--- src/MyPackage/Trading.java
+++ src/MyPackage/Trading.java
@@ -5,6 +5,7 @@ public class Trading {
 	public static void main(String[] args) {
 		
 		System.out.println("this is a second test case for Trading");
+		System.out.println("this is a Third test case for Trading");
 
 	}
 
