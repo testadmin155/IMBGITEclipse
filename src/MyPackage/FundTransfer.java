5a6f1cfa3daaf170fd1d9d13f1af8cbd92b82a7b
Anees <skanees007@gmail.com>
2023-07-07 06:50:11 +0200

    My First COmmit

---
+++ src/MyPackage/FundTransfer.java
@@ -0,0 +1,10 @@
+package MyPackage;
+
+public class FundTransfer {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a test case for Fund Transfer");
+	}
+
+}
---
+++ src/MyPackage/GetBalance.java
@@ -0,0 +1,10 @@
+package MyPackage;
+
+public class GetBalance {
+
+	public static void main(String[] args) {
+		System.out.println("this is a test case for get balance");
+
+	}
+
+}
