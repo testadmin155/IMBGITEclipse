7c88c1b3428ecf4648e9b60638df059b06e5b3e2
Anees <skanees007@gmail.com>
2023-07-07 07:12:21 +0200

    Trading branch

--- src/MyPackage/Trading.java
+++ src/MyPackage/Trading.java
@@ -4,6 +4,7 @@ public class Trading {
 
 	public static void main(String[] args) {
 		System.out.println("this is a test case for Trading");
+		System.out.println("this is a test case for Tranding Branch");
 
 	}
 
